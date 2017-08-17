01bed4c6a43a65d2b48ade934974bc581c344f38
boheningmeng <3053218092@qq.com>
2017-08-17 20:25:40 +0800

    增_后端 上传文件功能 在applicationContext.xml里面配置文件解释器

--- src/main/java/cn/lh/controller/CategoryController.java
+++ src/main/java/cn/lh/controller/CategoryController.java
@@ -9,9 +9,13 @@ import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 import java.util.List;
 
 // 告诉spring mvc这是一个控制器类
@@ -45,6 +49,23 @@ public class CategoryController {
         modelAndView.addObject("page",pageInfo);
         modelAndView.setViewName("/WEB-INF/jsp/list.jsp");
         return modelAndView;
+    }
+    @RequestMapping(value="/upload",method= RequestMethod.POST)
+    public String upload(MultipartFile file, HttpServletRequest request) {
+        String path = request.getSession().getServletContext().getRealPath("upload");
+        String fileName = file.getOriginalFilename();
+        File dir = new File(path,fileName);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        //MultipartFile自带的解析方法
+        try {
+            System.out.println(dir);
+            file.transferTo(dir);
+        }catch(Exception e){
+            return "failure.jsp";
+        }
+        return "success.jsp";
     }
 
 
--- src/main/webapp/success.jsp
+++ src/main/webapp/success.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: LENOVO
   Date: 2017/8/17
-  Time: 19:31
+  Time: 20:11
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,9 +11,6 @@
     <title>Title</title>
 </head>
 <body>
-<form action="/upload" method="post" enctype="multipart/form-data">
-    选择文件:<input type="file" name="file" width="120px">
-    <input type="submit" value="上传">
-</form>
+上传到本地项目成功
 </body>
 </html>
